c816d408852218be5a73f256581b11025e0349c7
Arvin Sharma <sharma.arv@husky.neu.edu>
2023-07-10 16:55:07 -0400

    update 2

--- least_squares.py
+++ least_squares.py
@@ -3,8 +3,19 @@ import random
 from matplotlib import pyplot as plt
 
 # Least square estimation with 1 degree polynomial
-def l_sq_1_deg(p):
-    pass
+def l_sq_1_deg(X, Y):
+    daX = X.T.dot(X)
+    daY = X.T.dot(np.ones((X.shape[0], 1)))[0]
+
+    dbX = daY
+    dbY = X.shape[0]
+    
+    rA = X.T.dot(Y)
+    rB = Y.T.dot(np.ones((Y.shape[0], 1)))[0]
+    A = np.array([[daX, dbX], [daY, dbY]])
+    res = np.linalg.solve(A, np.array([rA, rB]).reshape(2, 1))
+    return res[0], res[1] # a/m, b
+
 
 def _y(m, x, b):
     return m*x + b
@@ -17,6 +28,9 @@ def gen_linear(m, b, err=5):
     return X,Y
 
 X, Y = gen_linear(1, 25)
+m, b = l_sq_1_deg(X, Y)
 plt.plot(X, Y, "bo")
+Y_p = (X * m) + b
+plt.plot(X, Y_p, "r")
 plt.show()
 
