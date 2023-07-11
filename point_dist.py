550b5df0e91cbd49cdb3bac6917776a145e8dbea
Arvin Sharma <sharma.arv@husky.neu.edu>
2023-07-11 00:57:41 -0400

    add perp func

--- point_dist.py
+++ point_dist.py
@@ -14,37 +14,33 @@ def dist_k(x,y):
 def compute_perp_dist(X,Y, m, b):
     # compute and show perpendicular distance for each point to
     # best fit line (m, b)
-    # center axis to 0,0
-    yoff = -1*b
-    xoff = b / m
-    X = X - xoff
-    Y = Y - yoff
-    theta = np.arctan(m)
+    X = X
+    Y = Y
+    Yoff = b
+    n = X.shape[0]
     mx = np.max(X) * 1.25
     my = _y(m, mx, 0) # compute end point for L vector given M,B for proj 
-    plt.plot(mx, my, "go")
-    lu = np.array([[mx], [my]]).reshape((2,1)) / dist_k(mx, my) # Unit vector of L 
+    Lvec = np.array([[mx], [my]]).reshape((2,1))
+    Lu = Lvec / dist_k(mx, my) # Unit vector of L 
+    tvec = np.hstack((X, Y)).reshape(n, 2)
+    distxy = dist(X, Y).reshape(n, 1) # Composited vector containing magnitude xi,yi
     pt = np.array([[40], [80]]).reshape(2,1)
-    co = pt.T.dot(np.array([[mx], [my]]).reshape((2,1))) / (dist_k(40,80)*dist_k(mx, my))
-    npt = (dist_k(40, 80) * co) * lu
-    npt[1] = (npt[0] * m) + b
-    plt.plot(npt[0], npt[1], "g+")
+    costheta_v = np.divide(tvec.dot(Lvec), distxy * np.linalg.norm(Lvec))
+    costheta = np.divide(pt.T.dot(Lvec), (np.linalg.norm(pt) * np.linalg.norm(Lvec)))
+    proj_v = distxy * costheta * np.array([Lu]).reshape(1,1,2) + np.array([0, Yoff[0]]).reshape(1,1,2)
+    print("Lu ", Lu)
+    projpt = np.linalg.norm(pt) * costheta * Lu
+    projpt += np.array([0, Yoff[0]]).reshape((2,1))
+    plt.plot([pt[0], projpt[0]], [pt[1], projpt[1]], "g+-")
 
-    proj = (dist(X,Y) * np.cos(theta)).reshape(X.shape[0], 1)
-    proj = proj * np.array([lu]).reshape(1,1,2).T #+ np.array([[[xoff], [yoff]]]).reshape(1,1,2)).T
-    # Todo: vectorize this
-    #resid = []
-    #L = np.array([[mx], [my]]).reshape(2, 1)
-    #for p in proj:
-    #    resid.append(p * L)
-    X = X + xoff
-    Y = Y + yoff
-    return proj
+    print("Proj ", projpt, projpt / np.linalg.norm(projpt))
+    return proj_v
 
 
 def plot_resids(perps, X, Y):
     # plots the line formed from (X,Y)i -> Perp_i
-    #plt.plot(perps[0], perps[1], "go")
+    for ii, p in enumerate(perps[0]):
+       plt.plot([X[ii], p[0]], [Y[ii], p[1]], "go-")
     pass
 
 
@@ -54,11 +50,11 @@ def _y(m, x, b):
 
 def gen_linear(m, b, err=5):
     ## Generate linearly correlated points
-    X = np.linspace(0, 100, 500)
+    X = np.linspace(0, 100, 10)
     Y = np.array([_y(m, x, b) + random.gauss(-1*err,err) for x in X])
     return X,Y
 
-X, Y = gen_linear(1, 0)
+X, Y = gen_linear(1, 0, 50)
 m, b = l_sq_1_deg(X, Y)
 #plt.plot(X, Y, "bo")
 Y_p = (X * m) + b
